33a3ece0e4010b215671426fc4dbfdc3fab5289d
RicardoPereiraDev <odracirDavidPereira@gmail.com>
2025-12-24 17:44:50 +0000

    Save PersonDepartmentDTO

--- aula-salvar-para-um/src/main/java/com/devsuperior/aula/services/PersonService.java
+++ aula-salvar-para-um/src/main/java/com/devsuperior/aula/services/PersonService.java
@@ -3,6 +3,7 @@ package com.devsuperior.aula.services;
 import com.devsuperior.aula.dto.PersonDepartmentDTO;
 import com.devsuperior.aula.entities.Department;
 import com.devsuperior.aula.entities.Person;
+import com.devsuperior.aula.repositories.DepartmentRepository;
 import com.devsuperior.aula.repositories.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,6 +13,9 @@ public class PersonService {
     @Autowired
     private PersonRepository personRepository;
 
+    @Autowired
+    private DepartmentRepository departmentRepository;
+
     //metodo para retornar um PersonDepartmentDTO, e que vai receber argumentos
     public PersonDepartmentDTO insert(PersonDepartmentDTO dto){
 
@@ -19,8 +23,11 @@ public class PersonService {
         person.setName(dto.getName());
         person.setSalary(dto.getSalary());
 
-        Department department = new Department();
-        department.setId(dto.getDepartmentDTO().getId());
+        Department department= departmentRepository.getReferenceById(dto.getDepartmentDTO().getId());
+
+        //  Estas 2 linhas de codigo abaixo significam que criam um objecto "PersonDepartmentDTO" mas sem o nome do Department
+        //Department department = new Department();
+        //department.setId(dto.getDepartmentDTO().getId());
 
         person.setDepartment(department);
 
